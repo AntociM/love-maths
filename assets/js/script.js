81b098ece26464ecbd8cfbdd5f19cae9ded45c34
AntociM <gidioimihaela@gmail.com>
2021-11-01 19:35:45 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,38 @@
-function runGame() {
+// Wait for the DOM to finish loading before running the game
 
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for(let button of buttons){
+        button.addEventListener("click" , function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!")
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        })
+    }
+
+    runGame("addition");
+})
+
+/**
+ * The main game loop
+ */
+
+function runGame(gameType) {
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert (`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
+
 
 function checkAnswer() {
 
@@ -18,14 +50,17 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestions() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
-function displaySubstractQuestions() {
+function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestions() {
-    
+function displayMultiplyQuestion() {
+
 }
